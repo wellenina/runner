447ffe0279406f2c39a8d7ccb43a3fc6fac9a886
wellenina <giulia.fanzaghi@gmail.com>
2023-01-13 19:19:10 +0100

    Add game over and game states

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
+let BACKGROUND_SPEED = 1;
+let FOREGROUND_SPEED = 6;
+
+
 const runnerContainer = document.getElementById('runner-container');
 
 const background = {
     element: document.getElementById('background'),
-    speed: 1,
+    speed: BACKGROUND_SPEED,
     loopingPoint: 600
 }
 
 const ground = {
     element: document.getElementById('ground'),
-    speed: 6,
+    speed: FOREGROUND_SPEED,
     loopingPoint: 600
 }
 
 const dino = {
     element: document.getElementById('dino'),
-    dinoIdle: 'images/dino-idle.png',
-    dinoRun1: 'images/dino-run1.png',
-    dinoRun2: 'images/dino-run2.png',
-    dinoJump: 'images/dino-jump.png',
-    isJumping: false
-}
+    imgIdle: 'images/dino-idle.png',
+    imgRun1: 'images/dino-run1.png',
+    imgRun2: 'images/dino-run2.png',
+    imgJump: 'images/dino-jump.png',
+    imgDead: 'images/dino-dead.png',
+    isJumping: false,
+
+
+    run() {
+        if (this.element.getAttribute('src') === this.imgRun1) {
+            this.element.setAttribute('src', this.imgRun2)
+        } else {
+            this.element.setAttribute('src', this.imgRun1)
+        }
+    },
+    
+    jump() {
+        this.isJumping = true;
+        this.element.className = 'jumping';
+        this.element.setAttribute('src', this.imgJump);
+    },
+
+    end() {
+        this.element.setAttribute('src', this.imgDead);
+    }
+};
+
+dino.element.addEventListener('animationend', () => {
+    dino.isJumping = false;
+    dino.element.className = 'running';
+});
 
 const score = {
     INITIAL: '00000',
@@ -37,50 +66,61 @@ const score = {
     }
 }
 
+window.addEventListener('keydown', keyPressed);
+let playingTime;
+let gameState = 'start';
 
-let playingInterval;
-// when starting the game:
-playing = setInterval(update, 100);
+function keyPressed(event) {
+    if (event.key === ' ' || event.key === 'Spacebar' || event.key === 'ArrowUp' || event.key === 'Up') {
+        switch(gameState) {
+            case 'start': // questo succede solo alla prima partita
+              gameState = 'play'; // cambio stato
+              document.getElementById('game-start').style.display = 'none'; // tolgo il testo iniziale
+              display(background); // faccio apparire lo sfondo
+              display(ground);
+              playingTime = setInterval(update, 100); // faccio partire il ciclo di update
+              break;
+            case 'play':
+              dino.jump();
+              break;
+            case 'over':
+              gameState = 'play'; // cambio stato
+              document.getElementById('game-over').style.display = 'none'; // tolgo il testo 'game over'
+              score.reset(); // resetto il punteggio
+              playingTime = setInterval(update, 100); // faccio RIpartire il ciclo di update
+              // resettare difficoltà, cioè velocità e frequenza degli ostacoli
+          }
+    }
+};
 
-// game over:
-// clearInterval(playing);
+window.addEventListener('blur', () => {
+    clearInterval(playingTime);
+});
+window.addEventListener('focus', () => {
+    playingTime = setInterval(update, 100);
+});
+
 
 function update() {
    move(background);
    move(ground);
-   if (!dino.isJumping) { run(); };
+   if (!dino.isJumping) { dino.run(); };
    obstacles.spawn();
    obstacles.move();
-    score.increment();
+   score.increment();
+   if (detectCollision()) { endGame(); };
+}
+
+function display(obj) {
+    obj.element.style.left = 0;
+    obj.element.style.display = 'block';
 }
 
 function move(obj) {
-    if (!obj.element.style.left) { obj.element.style.left = 0 }; // questo si potrebbe settare all'inizio della partita
     obj.element.style.left = `${(parseFloat(obj.element.style.left) - obj.speed) % obj.loopingPoint}px`;
 }
 
-function run() {
-    if (dino.element.getAttribute('src') === dino.dinoRun1) {
-        dino.element.setAttribute('src', dino.dinoRun2)
-    } else {
-        dino.element.setAttribute('src', dino.dinoRun1)
-    }
-}
 
-window.addEventListener('keydown', jump);
-
-function jump(event) {
-    if (event.key === ' ' || event.key === 'Spacebar' || event.key === 'ArrowUp' || event.key === 'Up') {
-        dino.isJumping = true;
-        dino.element.className = 'jumping';
-        dino.element.setAttribute('src', dino.dinoJump);
-    }
-}
-
-dino.element.addEventListener('animationend', () => {
-    dino.isJumping = false;
-    dino.element.className = 'running';
-});
 
 
 
@@ -125,3 +165,15 @@ const obstacles = {
         }          
     }
 };
+
+function detectCollision() { // will return true or false
+    // there will be some awesome code here
+    return false;
+}
+
+function endGame() {
+    gameState = 'over';
+    clearInterval(playingTime);
+    document.getElementById('game-over').style.display = 'block';
+    dino.end();
+}
